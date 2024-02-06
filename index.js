bec9259d00dc561a04342ad735f5bfa9e7d36d9f
Rachel Atkinson <r.d.atkinson97@gmail.com>
2024-02-06 19:41:08 +0000

    Function to generate markdown for README

--- index.js
+++ index.js
@@ -65,3 +65,43 @@ async function writeToFile(fileName, data) {
         console.error(err);
     }
 }
+
+// Function to generate markdown for README
+function generateMarkdown(data) {
+    return `
+# ${data.title}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+\`\`\`
+${data.installation}
+\`\`\`
+
+## Usage
+${data.usage}
+
+## License
+This project is licensed under the ${data.license} license.
+
+## Contributing
+${data.contributing}
+
+## Tests
+\`\`\`
+${data.tests}
+\`\`\`
+
+## Questions
+For any questions, please contact me at [${data.email}](mailto:${data.email}) or visit my GitHub profile at [${data.github}](https://github.com/${data.github}).
+    `;
+}
